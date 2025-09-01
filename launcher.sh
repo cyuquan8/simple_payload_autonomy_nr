e3348c9f0172688189c3387a9a23f1281c81ca14
Chong Yu Quan <cyuquan8@gmail.com>
2025-09-02 06:57:49 +0800

    feat: add launcher script

---
+++ launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/useradmin/simple_payload_autonomy_nr
+
+# Start WFB service
+sudo systemctl start wifibroadcast@drone
+
+# Buffer
+sleep 30
+
+# Run with venv python directly (no need for activate)
+# Simple payload drone
+sudo venv/bin/python simple_drone_payload.py --detect-classes 0 2 4 7
